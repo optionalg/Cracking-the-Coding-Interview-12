e310ed5df7b5cdb1c9444746391d9a263a4fdc24
sammanthp007 <samman.thapa@bison.howard.edu>
2016-01-07 17:59:18 -0500

    rotate_image.py 1.6: undestanding the concept of getting the item at certain positions in a table defined by multi-dimentional array

---
+++ rotate_image.py
@@ -0,0 +1,33 @@
+# Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+
+
+lis = [[1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9,10,11,12],
+        [13,14,15,16]]
+
+def rotate_matrix(lis):
+    for layer in range(len(lis) // 2): # since we are swapping items so we only need to do for half the rows
+        first_considered = layer
+        last_considered = len(lis) - 1 - layer
+        # we swap value firstly the four values at the corner, then the four values at second corners and so on.
+        for index in range(first_considered, last_considered):
+            offset = index - first_considered
+            # save the top lefts value
+            top = lis[first_considered][index]
+            # swap the value of top_left with bottom_left
+            lis[first_considered][index] = lis[last_considered - offset][first_considered]
+            # print (lis)
+            # swap the value of bottom_left with bottom_right
+            lis[last_considered - offset][first_considered] = lis[last_considered][last_considered - offset]
+            # print (lis)
+            # swap the value of the bottom_right with top_right
+            lis[last_considered][last_considered - offset] = lis[index][last_considered]
+            # print (lis)
+            # swap the value of the top_right with the value stored in top
+            lis[index][last_considered] = top
+            # print (lis)
+
+print (lis)
+rotate_matrix(lis)
+print (lis)
