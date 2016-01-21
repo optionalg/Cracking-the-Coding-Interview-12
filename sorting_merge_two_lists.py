3405caf12a741829205bb9b4b25a686fcc0370c1
sammanthp007 <samman.thapa@bison.howard.edu>
2016-01-21 15:57:33 -0500

    sorting_merge_two_lists.py 9.1: add, start merging to the bigger list from behing

---
+++ sorting_merge_two_lists.py
@@ -0,0 +1,43 @@
+# Pythonic way -> costs a lot of space O(n ** 3)
+def merge(lisA, lisB):
+    if not lisA and not lisB:
+        return
+    if not lisA:
+        return lisB
+    if not lisB:
+        return lisA
+    if lisA[0] > lisB[0]:
+        return [lisB[0]] + merge(lisA, lisB[1:])
+    else:
+        return [lisA[0]] + merge(lisA[1:], lisB)
+
+lis1 = [3,4,5,66,77,88]
+lis2 = [6,8,9,12,454,623]
+# print (merge(lis1, lis2))
+
+# Non-pythonic, and pointer oriented
+def merge(lisA, lisB):
+    j = len(lisB) - 1
+    k = len(lisA) - 1
+    i = k - j - 1
+    while i >= 0 and j >= 0:
+        # case 1 ->  
+        if lisA[i] > lisB[j] : 
+            lisA[k] = lisA[i]
+            i -= 1
+            k -= 1
+        #case 2 -> 
+        if lisB[j] >= lisA[i]: 
+            lisA[k] = lisB[j]
+            j -= 1
+            k -= 1
+
+    while j >= 0:
+        lisA[k] = lisB[j]
+        j -= 1
+        k -= 1
+
+lisa = [2,4,6,8,0,0,0,0]
+lisb = [1,3,5,7]
+merge(lisa, lisb)
+print (lisa)
